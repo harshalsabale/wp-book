f4b4da8ce5375da6125b523f950604357cab8d00
harshalsabale <30774666+harshalsabale@users.noreply.github.com>
2021-11-29 12:13:30 +0530

    add Custom post type "Book"

---
+++ includes/class-wp-book-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://example.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ * @author     Harshal Sabale <harshalsabale004@gmail.com>
+ */
+class Wp_Book_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public function activate() {
+		
+	}
+
+}
